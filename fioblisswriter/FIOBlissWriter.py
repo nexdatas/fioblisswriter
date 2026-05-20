2159af6de401fd5a40230159db4f2e2456377d92
Jan Kotanski <jankotan@gmail.com>
2026-05-20 19:31:55 +0200

    move scan_write to a separate thread

--- fioblisswriter/FIOBlissWriter.py
+++ fioblisswriter/FIOBlissWriter.py
@@ -19,6 +19,7 @@ from tango import DebugIt, DevState
 from tango.server import Device
 # from tango.server import DevStatus
 from tango.server import command
+from tango.server import attribute
 from tango.server import device_property
 
 from .FIOWriterService import FIOWriterService as NWS
@@ -100,6 +101,17 @@ class FIOBlissWriter(Device):
         doc="sleep time between write_point command calls"
     )
 
+    # ----------
+    # Attributes
+    # ----------
+
+    Errors = attribute(
+        dtype=('DevString', ),
+        max_dim_x=1024,
+        label="errors",
+        doc="list of errors",
+    )
+
     # ---------------
     # General methods
     # ---------------
@@ -119,6 +131,13 @@ class FIOBlissWriter(Device):
 
     def dev_status(self):
         return self.fio_writer_service.get_status()
+
+    # ------------------
+    # Attributes methods
+    # ------------------
+
+    def read_Errors(self):
+        return self.nxs_writer_service.errors()
 
     # --------
     # Commands
--- fioblisswriter/FIOWriterService.py
+++ fioblisswriter/FIOWriterService.py
@@ -22,6 +22,7 @@
 import time
 
 import weakref
+import threading
 from blissdata.redis_engine.store import DataStore
 from blissdata.redis_engine.scan import ScanState
 from blissdata.redis_engine.exceptions import EndOfStream
@@ -60,6 +61,8 @@ class FIOWriterService:
         self._streams = StreamSet(weakref.ref(server) if server else None)
         #: (:obj:`bool`) service running flag
         self.__running = False
+        #: (:obj:`bool`) service error flag
+        self.__error = False
         #: (:obj:`int`) scan timeout in seconds
         self.__next_scan_timeout = next_scan_timeout
         self.__skip_final_parameters = skip_final_parameters
@@ -71,49 +74,143 @@ class FIOWriterService:
         self.__session = session
         #: (:class:`blissdata.redis_engine.store.DataStore`) datastore
         self.__datastore = DataStore(redis_url)
+        #: (:obj:`list`<:obj:`str`>) error list
+        self.__errors = []
+        #: (:class:`threading.Lock`) threading lock
+        self.__error_lock = threading.Lock()
+        #: (:obj:`dict`<:obj:`str`, :class:`ScanWriter`>) scan writers
+        self.__sws = {}
 
     def start(self):
         """ start writer service
         """
         self.__running = True
         timestamp = None
+        self.__error = False
+        self.__errors = []
 
         while self.__running:
             try:
-                timestamp, key = self.__datastore.get_next_scan(
-                    since=timestamp, timeout=self.__next_scan_timeout
-                )
-            except NoScanAvailable:
-                continue
-            scan = self.__datastore.load_scan(key)
-            if self.__session in [scan.session, "__all__"]:
-                self.write_scan(scan)
+                try:
+                    timestamp, key = self.__datastore.get_next_scan(
+                        since=timestamp, timeout=self.__next_scan_timeout
+                    )
+                except NoScanAvailable:
+                    continue
+                scan = self.__datastore.load_scan(key)
+                if self.__session in [scan.session, "__all__"]:
+                    # self.write_scan(scan)
+                    self.join_scans()
+                    sw = ScanWriter(
+                        scan, self._streams, self.__next_scan_timeout,
+                        self.__skip_final_parameters,
+                        self.__max_string_parameter_size,
+                        self.__snapshot_blacklist, self.__point_sleep_time
+                    )
+                    self.__sws[key] = sw
+                    sw.start()
+            except Exception as e:
+                self.__error = True
+                with self.__error_lock:
+                    self.__errors.append(str(e))
+
+    def joint_scans(self, stop=False):
+        """ join scans  which are stopped
+
+        :param stop: stop all scans flag
+        :type stop: :obj:`bool`
+        """
+        for key in list(self.__sws.keys()):
+            sw = self.__sws.pop(key)
+            if stop:
+                sw.running = False
+            if not sw.running:
+                if self.error:
+                    with self.__error_lock:
+                        self.__errors.extend(self.errors[:])
+                sw.join()
 
     def get_status(self):
         """ get writer service status
         """
-        status = "is RUNNING" if self.__running else "is STOPPED"
+        if self.__error:
+            status = "is FAILED"
+        else:
+            status = "is RUNNING" if self.__running else "is STOPPED"
         return "FIOBlissWriter %s" % status
 
     def stop(self):
         """ stop writer service
         """
         self.__running = False
+        self.joint_scans(stop=True)
 
-    def write_scan(self, scan):
-        """ write scan data
-
-        :param scan: blissdata scan
-        :type scan:
+    def errors(self):
+        """ list of errors
         """
-        while scan.state < ScanState.PREPARED:
+        errors = []
+        with self.__error_lock:
+            errors = self.__errors[:]
+        return errors
+
+
+class ScanWriter(threading.Thread):
+
+    def __init__(self, scan, streams, next_scan_timeout,
+                 skip_final_parameters=False, max_string_parameter_size=300,
+                 snapshot_blacklist=None, point_sleep_time=0.01):
+        """ constructor
+
+        :param scan: blissdata redis url
+        :type scan: :class:`Scan`
+        :param streams: tango streams
+        :type streams: :class:`StreamSet` or :class:`tango.LatestDeviceImpl`
+        :param next_scan_timeout: timeout  between the scans in seconds
+        :type next_scan_timeout: :obj:`int`
+        :param skip_final_parameters: skip final parameters
+        :type skip_final_parameters: :obj:`bool`
+        :param max_string_parameter_size: maximal string parameter size
+        :type max_string_parameter_size: :obj:`int`
+        :param snapshotblacklist: snapshot blacklist
+        :type snapshotblacklist: :obj:`list` <:obj:`str`>
+        :param point_sleep_time: sleep time between write point calls
+        :type point_sleep_time: :obj:`float`
+        :param server: NXSConfigServer instance
+        :type server: :class:`tango.LatestDeviceImpl`
+
+        """
+        #: (:class:`Scan`) blissdata scan
+        self._scan = scan
+        #: (:class:`StreamSet` or :class:`tango.LatestDeviceImpl`) stream set
+        self._streams = streams
+        #: (:obj:`bool`) service running flag
+        self.running = False
+        #: (:obj:`bool`) service error flag
+        self.error = False
+        #: (:obj:`int`) scan timeout in seconds
+        self.__next_scan_timeout = next_scan_timeout
+        self.__skip_final_parameters = skip_final_parameters
+        self.__max_string_parameter_size = max_string_parameter_size
+        self.__snapshot_blacklist = snapshot_blacklist
+        self.__point_sleep_time = point_sleep_time
+
+        #: (:obj:`list`<:obj:`str`>) error list
+        self.__errors = []
+        #: (:class:`threading.Lock`) threading lock
+        self.error_lock = threading.Lock()
+
+    def run(self):
+        """ write scan data
+        """
+        self.running = True
+        while self._scan.state < ScanState.PREPARED and self.running:
             time.sleep(self.__point_sleep_time)
-            scan.update()
+            self._scan.update()
         self._streams.info(
-            "FIOWriterService::write_scan CREATE FILE: %s" % scan.number)
+            "FIOWriterService::write_scan CREATE FILE: %s" % self._scan.number)
 
         fiofl = create_fio_file(
-            scan,
+            self._scan,
             self._streams,
             self.__skip_final_parameters,
             self.__max_string_parameter_size,
@@ -124,30 +221,31 @@ class FIOWriterService:
             return
 
         self._streams.info(
-            "FIOWriterService::write_scan INIT: %s" % scan.number)
+            "FIOWriterService::write_scan INIT: %s" % self._scan.number)
         fiofl.write_init_snapshot()
 
         fiofl.prepareChannels()
 
-        while self.__running:
+        while self.running:
             try:
-                scan.update(block=False)
+                self._scan.update(block=False)
                 self._streams.debug(
                     "FIOWriterService::write_scan SCAN POINT: %s"
-                    % scan.number)
+                    % self._scan.number)
                 fiofl.write_scan_points()
                 time.sleep(self.__point_sleep_time)
             except EndOfStream:
                 break
 
-        while scan.state < ScanState.CLOSED:
+        while self._scan.state < ScanState.CLOSED and self.running:
             time.sleep(self.__point_sleep_time)
-            scan.update()
+            self._scan.update()
 
         self._streams.info(
-            "FIOWriterService::write_scan FINAL: %s" % scan.number)
+            "FIOWriterService::write_scan FINAL: %s" % self._scan.number)
         fiofl.write_final_snapshot()
         fiofl.close()
+        self.running = False
 
 
 def main():
